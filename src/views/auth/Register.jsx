38c3c42a76a39c6654216e752874a858fb2ab5c2
Vivek More <80022013+mrvivekmore@users.noreply.github.com>
2024-06-21 11:36:50 +0530

    Login Register page state define

--- src/views/auth/Register.jsx
+++ src/views/auth/Register.jsx
@@ -1,7 +1,26 @@
 import { Link } from "react-router-dom";
 import { FaGoogle, FaFacebook } from "react-icons/fa";
+import { useState } from "react";
 
 const Register = () => {
+  const [state, setState] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  const inputHandle = (e) => {
+    setState({
+      ...state,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const submit = (e) => {
+    e.preventDefault();
+    console.log(state);
+  };
+
   return (
     <div className="min-w-screen min-h-screen bg-[#cdcae9] flex justify-center items-center">
       <div className="w-[350px] text-[#ffffff] p-2">
@@ -11,10 +30,12 @@ const Register = () => {
             Please register your account
           </p>
 
-          <form>
+          <form onSubmit={submit}>
             <div className="flex flex-col w-full gap-1 mb-3">
               <label htmlFor="name">Name</label>
               <input
+                onChange={inputHandle}
+                value={state.name}
                 className="px-3 py-2 outline-none border border-slate-400 bg-transparent rounded-md"
                 type="text"
                 name="name"
@@ -27,6 +48,8 @@ const Register = () => {
             <div className="flex flex-col w-full gap-1 mb-3">
               <label htmlFor="email">Email</label>
               <input
+                onChange={inputHandle}
+                value={state.email}
                 className="px-3 py-2 outline-none border border-slate-400 bg-transparent rounded-md"
                 type="email"
                 name="email"
@@ -39,6 +62,8 @@ const Register = () => {
             <div className="flex flex-col w-full gap-1 mb-3">
               <label htmlFor="password">Password</label>
               <input
+                onChange={inputHandle}
+                value={state.password}
                 className="px-3 py-2 outline-none border border-slate-400 bg-transparent rounded-md"
                 type="password"
                 name="password"
